c3841877cc05b1c158e90a1a74eb87092dbc05ba
odisseus <m9olub@gmail.com>
2017-12-26 12:44:05 +0100

    Added HOCON.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,4 +2,5 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.3"
+  lazy val hocon = "com.typesafe" % "config" % "1.3.1"
 }
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,7 +1,11 @@
 package example
 
+import com.typesafe.config.ConfigFactory
+
 object Hello extends Greeting with App {
   println(greeting)
+  val config = ConfigFactory.load("lang-uk.conf")
+  println(config.getStringList("months"))
 }
 
 trait Greeting {
