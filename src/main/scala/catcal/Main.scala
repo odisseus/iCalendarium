680fb47134dae74180baae0c4eb62158a89e7cab
odisseus <m9olub@gmail.com>
2018-03-05 23:04:40 +0100

    Added output to iCalendar file.

--- src/main/scala/catcal/Main.scala
+++ src/main/scala/catcal/Main.scala
@@ -9,13 +9,18 @@ object Main extends Core with App {
   val config = ConfigFactory.load("lang-uk.conf")
   val currentYear = DateTime.now().getYear
 
-  val sourcesPath = args.headOption.getOrElse(".")
+  val sourcesPath = args.applyOrElse[Int, String](0, _ => ".")
+  val targetPath = args.applyOrElse[Int, String](1, _ => "output.ics")
+
   val sourcesDirectory = new File(sourcesPath)
   val sourceFiles = sourcesDirectory.list().filter(_.endsWith(".txt")).map(x => sourcesDirectory.toPath.resolve(x).toString)
   val events = sourceFiles.map(eventImporter.read).flatMap(_.get) ++ predefinedEvents(currentYear).events
   val resolvedEvents = eventResolver(currentYear, events).resolveAll()
 
-  resolvedEvents.foreach(println)
+  val icalendar = eventExporter.toICalendar(resolvedEvents)
+
+  val outputFile = new File(targetPath)
+  icalendar.write(outputFile)
 
   override def parserConfiguration = ParserConfiguration(config)
 
