d41ecc9975c3a20d5673ffa86c06f1d665f6bfbe
odisseus <m9olub@gmail.com>
2018-07-20 19:42:11 +0200

    Fixed the integration test.

--- src/test/scala/catcal/service/EventImporterTest.scala
+++ src/test/scala/catcal/service/EventImporterTest.scala
@@ -20,7 +20,7 @@ class EventImporterTest extends FlatSpec with Matchers {
   val eventImporter = new EventImporter(parser)
 
   it should "parse a valid event list from a reader" in {
-    val x = Source.fromURL(getClass.getResource("/fixed-days.txt"))
+    val x = Source.fromURL(getClass.getResource("/test-fixed-days.txt"))
     val (parsedList, errors) = eventImporter.read(x.reader()).get
     parsedList should have size 7
     errors shouldBe 'empty
